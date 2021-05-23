673e1cc25b0a10e760081f59b7674d4c3a510a11
Juli Cheng <juli.cheng@axiell.com>
2021-05-23 17:03:55 +0100

    Update firebase yml file

--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Wrapper({ children = null }) {
   return (
-    <div className='bg-blue-100 overflow-y-scroll h-full m-8 mx-auto w-9/12 md:w-6/12 rounded shadow flex justify-center flex-col items-center'>
+    <div className='bg-blue-100 overflow-y-scroll h-full m-8 mx-auto w-11/12 sm:w-9/12 md:w-8/12 lg:w-6/12 rounded shadow flex justify-center flex-col items-center'>
       {children}
     </div>
   );
